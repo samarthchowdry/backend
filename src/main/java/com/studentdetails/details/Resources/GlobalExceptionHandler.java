505b2116f09b156fb562ac4a22280ce0fe6ea46a
samarthchowdry <samarthchowdry1@gmail.com>
2025-10-20 14:50:43 +0530

    implementing  Mapper Class for smoother run of the backend application

---
+++ src/main/java/com/studentdetails/details/Resources/GlobalExceptionHandler.java
@@ -0,0 +1,4 @@
+package com.studentdetails.details.Resources;
+
+public class Global {
+}
--- src/main/java/com/studentdetails/details/Service/ServiceImpl/StudentServiceImpl.java
+++ src/main/java/com/studentdetails/details/Service/ServiceImpl/StudentServiceImpl.java
@@ -8,7 +8,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors; // ✅ Missing import added
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
